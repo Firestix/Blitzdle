aee1afb60dda4f0d83cbad3337525b83b870ba64
Max Crull <firestix45@gmail.com>
2026-02-14 23:35:04 -0500

    Title generation moved into module scope

--- modules/Title.js
+++ modules/Title.js
@@ -1,12 +1,13 @@
 import { GuessData } from "./GuessData.js";
 import { WordList } from "./WordList.js";
 
+const date = new Date();
+const seed = `9${date.getFullYear()}${(date.getMonth()+1).toString().padStart(2,"0")}${date.getDate().toString().padStart(2,"0")}`
+const wordlist = await WordList.fromURL("./wordleblitz.txt");
+const word = wordlist.randomize(1,seed)[0];
+const guessData = new GuessData(word,"BLITZ");
+
 export async function createTitle(elem) {
-    let date = new Date();
-    let seed = `9${date.getFullYear()}${(date.getMonth()+1).toString().padStart(2,"0")}${date.getDate().toString().padStart(2,"0")}`
-    let wordlist = await WordList.fromURL("./wordleblitz.txt");
-    let word = wordlist.randomize(1,seed)[0];
-    let guessData = new GuessData(word,"BLITZ");
     elem.appendChild(guessData.buildElement())
     elem.createChildNode("div",{class:"dle"},"DLE");
 }
